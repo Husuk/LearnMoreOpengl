058e3f9d8319d9a1ad65e3fb63af7f6992db0a9a
Husuk <huseyinarslan170210@gmail.com>
2025-12-09 12:31:21 +0300

    Update main.cpp

--- LearnMoreOpengl/main.cpp
+++ LearnMoreOpengl/main.cpp
@@ -82,12 +82,7 @@ int main(){
 	textureCompiler myTexture("C:/Users/user/source/repos/LearnMoreOpengl/LearnMoreOpengl/wall.jpg",GL_TEXTURE_2D,GL_MIRRORED_REPEAT,GL_MIRRORED_REPEAT,GL_LINEAR_MIPMAP_LINEAR,GL_LINEAR,GL_RGB);
 	
 
-	/*
-	*/
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	
 	
 	
 	
